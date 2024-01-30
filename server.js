483f2a6476f3748f2002348898b2acabfd66f5b3
Mariusz <mkgiepard@gmail.com>
2024-01-30 20:27:16 +0100

    new> add dedicated authentication server

--- server.js
+++ server.js
@@ -1,5 +1,10 @@
+require("dotenv").config();
+
 const express = require("express");
 const app = express();
+const jwt = require("jsonwebtoken");
+
+app.use(express.json());
 
 const posts = [
   {
@@ -12,8 +17,21 @@ const posts = [
   },
 ];
 
-app.get("/posts", (req, res) => {
-  res.json(posts);
+app.get("/posts", authenticateToken, (req, res) => {
+  res.json(posts.filter((post) => post.username === req.user.name));
 });
+
+// middleware
+function authenticateToken(req, res, next) {
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
+
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+}
 
 app.listen(3000);
