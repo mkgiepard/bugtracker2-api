9177fc2a07e0706e889de8d4ec6d10f370f9ca43
Mariusz <mkgiepard@gmail.com>
2024-04-18 13:36:16 +0200

    new> adding dummy /testing route, just for testing RESTAssured setup

--- routes/app.js
+++ routes/app.js
@@ -5,4 +5,8 @@ router.get("/settings", passport.authenticate("jwt", { session: false }), (req, 
   res.json({ msg: "SUCCESS: protected /settings route (3000)" });
 });
 
+router.get("/testing", (req, res) => {
+  res.json({ msg: "SUCCESS: not protected /testing route (3000)" });
+});
+
 module.exports = router;
