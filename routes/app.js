3922c34782c7619bea26d95a194765a06fd5b7b3
Mariusz <mkgiepard@gmail.com>
2024-04-23 13:43:28 +0200

    new> add support for /users /users/username, w/o middleware so far

--- routes/app.js
+++ routes/app.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const passport = require("passport");
+const mongoose = require("mongoose");
+const User = mongoose.model("User");
 
 router.get("/settings", passport.authenticate("jwt", { session: false }), (req, res) => {
   res.json({ msg: "SUCCESS: protected /settings route (3000)" });
@@ -7,6 +9,31 @@ router.get("/settings", passport.authenticate("jwt", { session: false }), (req, 
 
 router.get("/testing", (req, res) => {
   res.json({ msg: "SUCCESS: not protected /testing route (3000)" });
+});
+
+router.get("/users", (req, res) => {
+  User.find({})
+    .select(["-password", "-__v"])
+    .then(async (users) => {
+      return res.status(200).send(users);
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
+router.get("/users/:username", (req, res) => {
+  User.findOne({username: req.params.username})
+    .select(["-password", "-__v"])
+    .then(async (user) => {
+      if (!user) {
+        return res.status(404).send();
+      }
+      return res.status(200).send(user);
+    })
+    .catch((err) => {
+      next(err);
+    });
 });
 
 module.exports = router;
