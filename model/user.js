840a46b9c3d68e5ac0d944bfef0690b4203ff81b
Mariusz <mkgiepard@gmail.com>
2024-04-23 12:39:50 +0200

    new> update 'User' schema to match the one used in FE

--- model/user.js
+++ model/user.js
@@ -4,6 +4,8 @@ const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password: String,
+  firstName: String,
+  lastName: String,
 });
 
 mongoose.model("User", UserSchema);
